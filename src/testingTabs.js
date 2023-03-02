312dc98d8c740a064eeddfa4172d9492ec535f31
Brian Walters <waltersbrian68@gmail.com>
2023-03-02 16:53:39 -0500

    made progress on tabs section.

--- src/testingTabs.js
+++ src/testingTabs.js
@@ -11,6 +11,7 @@ import Slide from "@mui/material/Slide";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
+
   return (
     <div
       role="tabpanel"
@@ -113,167 +114,171 @@ function UserInput() {
     setChecked((prev) => !prev);
   };
 
-  //   ----Tabs Code
+  //   ----Tabs
 
   return (
-    <div>
-      <div>
-        <Tabs value={value} onChange={handleChange}>
+    <div className="user-input-container">
+      <div className="user-tabs">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="secondary"
+          variant="fullWidth"
+        >
           <Tab label="Item One" {...a11yProps(0)} onClick={handleClick} />
           <Tab label="Item Two" {...a11yProps(1)} onClick={handleClick} />
         </Tabs>
       </div>
-      <div className="user-input-container">
-        <TabPanel value={value} index={0}>
-          <Slide direction="right" in={checked} mountOnEnter unmountOnExit>
-            <div className="user-input-flex">
-              <div className="user-input-heading">
-                <AddCircleOutlineIcon color="secondary" />
-                <h2 className="user-input-title">Add Inventory Item</h2>
-              </div>
-              <TextField
-                variant="filled"
-                required
-                error={itemName === "" ? true : false}
-                helperText={
-                  itemName === ""
-                    ? "Please choose a name no longer than 20 characters"
-                    : ""
-                }
-                id="item-name"
-                label="Item Name"
-                placeholder="Item Name"
-                className="text-field"
-                color="secondary"
-                onChange={(event) => {
-                  user_input();
-                  setItemName(event.target.value.toUpperCase());
-                }}
-                inputProps={{ maxLength: 20, autoComplete: "off" }}
-                value={itemName}
-                onKeyDown={(event) => {
-                  if (!event.key.match(/[a-zA-Z0-9\s]/)) {
-                    event.preventDefault();
-                  }
-                }}
-                size="small"
-              />
-              <TextField
-                variant="filled"
-                required
-                error={itemCode === "" || itemCode.length < 8 ? true : false}
-                helperText={
-                  itemCode === "" || itemCode.length < 8
-                    ? "Please choose a code equal to 8 characters"
-                    : ""
-                }
-                id="item-code"
-                label="Item Code"
-                placeholder="Item Code"
-                className="text-field"
-                color="secondary"
-                onChange={(event) => {
-                  user_input();
-                  setItemCode(
-                    event.target.value.toUpperCase().split(" ").join("")
-                  );
-                }}
-                inputProps={{ maxLength: 8, autoComplete: "off" }}
-                value={itemCode}
-                onKeyDown={(event) => {
-                  if (!event.key.match(/[a-zA-Z0-9]/)) {
-                    event.preventDefault();
-                  }
-                }}
-                size="small"
-              />
-              <TextField
-                variant="filled"
-                required
-                error={
-                  itemQuantity === "" || Number(itemQuantity) > 10000
-                    ? true
-                    : false
-                }
-                helperText={
-                  itemQuantity === "" || Number(itemQuantity) > 10000
-                    ? "Please choose a number between 1 and 10000"
-                    : ""
-                }
-                id="item-quantity"
-                label="Quantity"
-                type="number"
-                placeholder="Quantity"
-                className="text-field"
-                color="secondary"
-                onChange={user_input}
-                inputProps={{ min: 0, max: 10000, autoComplete: "off" }}
-                value={itemQuantity}
-                size="small"
-              />
-              <TextField
-                variant="filled"
-                required
-                error={
-                  itemPrice === "" || Number(itemPrice) > 10000 ? true : false
-                }
-                helperText={
-                  itemPrice === "" || Number(itemPrice) > 10000
-                    ? "Please choose a number between 1 and 10000"
-                    : ""
-                }
-                id="item-price"
-                label="Item Price"
-                type="number"
-                placeholder="Item Price"
-                className="text-field"
-                color="secondary"
-                onChange={user_input}
-                inputProps={{ min: 0, max: 10000, autoComplete: "off" }}
-                value={itemPrice}
-                size="small"
-              />
-              <Alert
-                id="error-alert"
-                severity="error"
-                onClose={() => {
-                  document.getElementById("error-alert").style.display = "none";
-                }}
-                variant="filled"
-              >
-                <AlertTitle>Error</AlertTitle>
-                Invalid entry!
-              </Alert>
-              <Alert
-                id="success-alert"
-                severity="success"
-                onClose={() => {
-                  document.getElementById("success-alert").style.display =
-                    "none";
-                  setSuccessAlert("");
-                }}
-                variant="filled"
-              >
-                <AlertTitle>Success</AlertTitle>
-                {successAlert} has been added to your Inventory!
-              </Alert>
-              <Button
-                color="secondary"
-                className="button"
-                onClick={SubmitForm}
-                variant="contained"
-                startIcon={<AddCircleOutlineIcon />}
-              >
-                Submit
-              </Button>
-            </div>
-          </Slide>
-        </TabPanel>
 
-        <TabPanel value={value} index={1}>
-          <p>Bye</p>
-        </TabPanel>
-      </div>
+      <TabPanel value={value} index={0}>
+        <Slide direction="right" in={checked} mountOnEnter unmountOnExit>
+          <div className="user-input-flex">
+            <div className="user-input-heading">
+              <AddCircleOutlineIcon color="secondary" />
+              <h2 className="user-input-title">Add Inventory Item</h2>
+            </div>
+            <TextField
+              variant="filled"
+              required
+              error={itemName === "" ? true : false}
+              helperText={
+                itemName === ""
+                  ? "Please choose a name no longer than 20 characters"
+                  : ""
+              }
+              id="item-name"
+              label="Item Name"
+              placeholder="Item Name"
+              className="text-field"
+              color="secondary"
+              onChange={(event) => {
+                user_input();
+                setItemName(event.target.value.toUpperCase());
+              }}
+              inputProps={{ maxLength: 20, autoComplete: "off" }}
+              value={itemName}
+              onKeyDown={(event) => {
+                if (!event.key.match(/[a-zA-Z0-9\s]/)) {
+                  event.preventDefault();
+                }
+              }}
+              size="small"
+            />
+            <TextField
+              variant="filled"
+              required
+              error={itemCode === "" || itemCode.length < 8 ? true : false}
+              helperText={
+                itemCode === "" || itemCode.length < 8
+                  ? "Please choose a code equal to 8 characters"
+                  : ""
+              }
+              id="item-code"
+              label="Item Code"
+              placeholder="Item Code"
+              className="text-field"
+              color="secondary"
+              onChange={(event) => {
+                user_input();
+                setItemCode(
+                  event.target.value.toUpperCase().split(" ").join("")
+                );
+              }}
+              inputProps={{ maxLength: 8, autoComplete: "off" }}
+              value={itemCode}
+              onKeyDown={(event) => {
+                if (!event.key.match(/[a-zA-Z0-9]/)) {
+                  event.preventDefault();
+                }
+              }}
+              size="small"
+            />
+            <TextField
+              variant="filled"
+              required
+              error={
+                itemQuantity === "" || Number(itemQuantity) > 10000
+                  ? true
+                  : false
+              }
+              helperText={
+                itemQuantity === "" || Number(itemQuantity) > 10000
+                  ? "Please choose a number between 1 and 10000"
+                  : ""
+              }
+              id="item-quantity"
+              label="Quantity"
+              type="number"
+              placeholder="Quantity"
+              className="text-field"
+              color="secondary"
+              onChange={user_input}
+              inputProps={{ min: 0, max: 10000, autoComplete: "off" }}
+              value={itemQuantity}
+              size="small"
+            />
+            <TextField
+              variant="filled"
+              required
+              error={
+                itemPrice === "" || Number(itemPrice) > 10000 ? true : false
+              }
+              helperText={
+                itemPrice === "" || Number(itemPrice) > 10000
+                  ? "Please choose a number between 1 and 10000"
+                  : ""
+              }
+              id="item-price"
+              label="Item Price"
+              type="number"
+              placeholder="Item Price"
+              className="text-field"
+              color="secondary"
+              onChange={user_input}
+              inputProps={{ min: 0, max: 10000, autoComplete: "off" }}
+              value={itemPrice}
+              size="small"
+            />
+            <Alert
+              id="error-alert"
+              severity="error"
+              onClose={() => {
+                document.getElementById("error-alert").style.display = "none";
+              }}
+              variant="filled"
+            >
+              <AlertTitle>Error</AlertTitle>
+              Invalid entry!
+            </Alert>
+            <Alert
+              id="success-alert"
+              severity="success"
+              onClose={() => {
+                document.getElementById("success-alert").style.display = "none";
+                setSuccessAlert("");
+              }}
+              variant="filled"
+            >
+              <AlertTitle>Success</AlertTitle>
+              {successAlert} has been added to your Inventory!
+            </Alert>
+            <Button
+              color="secondary"
+              className="button"
+              onClick={SubmitForm}
+              variant="contained"
+              startIcon={<AddCircleOutlineIcon />}
+            >
+              Submit
+            </Button>
+          </div>
+        </Slide>
+      </TabPanel>
+
+      <TabPanel value={value} index={1}>
+        <p>Bye</p>
+      </TabPanel>
     </div>
   );
 }
